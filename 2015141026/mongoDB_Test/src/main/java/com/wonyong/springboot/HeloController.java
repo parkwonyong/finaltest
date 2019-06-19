4610cbdfec2b7c2001abf9965c8b40b195472a89
parwonyong <blakmini@naver.com>
2019-06-20 08:26:37 +0900

    주석8

--- 2015141026/mongoDB_Test/src/main/java/com/wonyong/springboot/HeloController.java
+++ 2015141026/mongoDB_Test/src/main/java/com/wonyong/springboot/HeloController.java
@@ -203,6 +203,24 @@ public class HeloController {
 		mav.addObject("datalist", list);
 		return mav;
 	}
+	
+	/**
+	 *
+	 * @fn 		public ModelAndView editpost(ModelAndView mav)
+	 * 
+	 * @brief 	데이터 수정
+	 *
+	 * @author 	박원용
+	 * @date 	2019-06-20
+	 *
+	 * @param 	mav ModelAndView
+	 *
+	 * @remark	폼에 입력된 값을 받아온 후 객체에 저장	[2019-06-20; 박원용] \n
+	 *		   	save 메소드를 이용해 DB에 저장		[2019-06-20; 박원용] \n
+	 *			deleteBy를 이용해 이전데이터 삭제	[2019-06-20; 박원용] \n
+	 *
+	 */
+	
 	@RequestMapping(value = "/edit", method = RequestMethod.POST)
 	public ModelAndView editpost(
 			@RequestParam("id") String id, 
