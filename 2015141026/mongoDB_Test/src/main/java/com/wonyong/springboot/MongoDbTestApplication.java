37afd37d219300701eefe3a09bbe794feb479f88
parwonyong <blakmini@naver.com>
2019-06-20 03:59:12 +0900

    MongoDbTestApplication.java

--- 2015141026/mongoDB_Test/src/main/java/com/wonyong/springboot/MongoDbTestApplication.java
+++ 2015141026/mongoDB_Test/src/main/java/com/wonyong/springboot/MongoDbTestApplication.java
@@ -6,4 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MongoDbTestApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbTestApplication.class, args);
+	}
+
 }
