f047a1b860986ff35f2b88925f4dfb6e789f509d
parwonyong <blakmini@naver.com>
2019-06-20 02:24:39 +0900

    get,set

--- 2015141026/mongoDB_Test/src/main/java/com/wonyong/springboot/MyDataMongo.java
+++ 2015141026/mongoDB_Test/src/main/java/com/wonyong/springboot/MyDataMongo.java
@@ -22,4 +22,46 @@ public class MyDataMongo {
 		this.memo =  memo;
 		this.number = number;
 	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+
 }
