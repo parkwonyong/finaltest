74b6b6cb8246de51a14934a12efd6061e0e76f78
parwonyong <blakmini@naver.com>
2019-06-20 05:02:55 +0900

    주석5

--- 2015141026/mongoDB_Test/src/main/java/com/wonyong/springboot/HeloController.java
+++ 2015141026/mongoDB_Test/src/main/java/com/wonyong/springboot/HeloController.java
@@ -129,6 +129,21 @@ public class HeloController {
 		return mav;
 	}
 	
+	/**
+	 *
+	 * @fn 		public ModelAndView removecheck(ModelAndView mav)
+	 * 
+	 * @brief 	삭제 유무 확인페이지
+	 *
+	 * @author  박원용
+	 * @date 	2019-06-20
+	 *
+	 * @param 	mav ModelAndView
+	 *
+	 * @remark	findBy 를 이용한 조건검색 후 삭제 유무 확인페이지 출력	[2019-06-20; 박원용] \n
+	 *
+	 */
+	
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public ModelAndView removecheck(@PathVariable("id") String id, ModelAndView mav) {
 		mav.setViewName("delete");
