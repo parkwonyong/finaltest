3af5b578451dbb9c638f779af2c7959897a8463f
parwonyong <blakmini@naver.com>
2019-06-20 02:07:12 +0900

    id,name,address,memo,number 필드선언

--- 2015141026/mongoDB_Test/src/main/java/com/wonyong/springboot/MyDataMongo.java
+++ 2015141026/mongoDB_Test/src/main/java/com/wonyong/springboot/MyDataMongo.java
@@ -5,5 +5,12 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 
 public class MyDataMongo {
+	
+	private String id;
+	
+	private String name;
+	private String address;
+	private String memo;
 
+	private int number;
 }
